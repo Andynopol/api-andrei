28c9271358cf9767bcc0aa8e8b3822ceeaa2643d
Andynopol <41628030+Andynopol@users.noreply.github.com>
2020-10-02 01:02:56 +0300

    ok

--- UI/app.js
+++ UI/app.js
@@ -1,50 +1,39 @@
-const sendRequest = function ( data ) {
-	const url = '/files';
-	console.log( data[0] );
-	const options = {
-		method: 'POST',
-		// headers: {
-		// 	'Content-Type': 'application/json'
-		// },
-		body: data[0]
-	};
-	fetch( url, options ).then( async ( response ) => console.log( await response.json() ) );
-};
 
 
 
-const getElem = function ( id ) {
-	return document.getElementById( id );
-};
+const main = function(){
+	
+	const form = document.getElementById('form');
 
-const main = function () {
-
-	const form = document.getElementById( 'form' );
-
-	form.addEventListener( 'submit', function ( e ) {
+	form.addEventListener('submit', function(e){
 		e.preventDefault();
+		const file = document.getElementById("input").files[0];
+		console.log(file);
+		const formData = new FormData();
+		formData.file = file;
+		console.log(formData);
+		fetch('/files', {method: "POST", body: formData});
+	});
+	
+}
 
 
-		// const files = document.querySelector( '[type=file]' ).files
-		// const formData = new FormData()
-		// formData.files = [];
-		// for ( let i = 0; i < files.length; i++ ) {
-		// 	let file = files[ i ];
 
-		// 	formData.files[ i ] = file;
-		// }
-		// console.log( formData );
-		// sendRequest( files );
 
-		const files = document.getElementById('files');
 
-		console.log(files);
-	} )
+
+
+
+
+
+
+
+
+const setup = function(){
+	console.log('We are ready to go');
+	main();
 };
 
-const setup = function () {
-	noCanvas();
-	// main();
-}
+
 
 window.setup = setup;
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+// const { request } = require( 'express' );
 const express = require('express');
-const multer = require('multer');
-const ejs = require('ejs');
-const path = require('path');
+const upload = require('express-fileupload');
 
 // Init app
 
 const app = express();
+
+app.use(upload());
 
 const port = 3000;
 
@@ -14,4 +15,9 @@ app.use( express.static( './UI' ) );
 app.listen(port, ()=>{
 	console.log(`Listening at port ${port}`);
 });
+
+
+app.post('/files', (req, res)=>{
+	console.log(req.body);
+})
 
