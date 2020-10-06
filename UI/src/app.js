59ff924146978ad1ca610d07618d61e6239837a2
Andynopol <41628030+Andynopol@users.noreply.github.com>
2020-10-06 04:25:44 +0300

    ok

--- UI/src/app.js
+++ UI/src/app.js
@@ -2,18 +2,50 @@ const axios = require('axios');
 
 const main = function () {
 	const url = '/files';
-	const files = document.getElementById( 'fileInput' );
+	const files = document.querySelector( '#fileInput' );
 	const submit = document.getElementById('upload');
 	submit.addEventListener('click', function(e){
-		const data = files.files;
-		const options = {};
-		HTTPRequest(url, data);
+		// const data = files.files;
+		// console.log(data);
+		// const options = {
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	},
+		// 	body: data
+		// };
+		// HTTPRequest(url, options);
+		const formData = defineFormData(files.files);
+		// console.log(formData);
+		// axios.post(url, formData, {
+		// 	headers: {
+		// 		'Content-Type': 'multipart/form-data'
+		// 	}
+		// });
+		const options = {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		};
+		HTTPRequest(url, formData, options);
 	});
 	
 }
 
-const HTTPRequest = async function(url, data, options){
+const defineFormData = function(files){
+	console.log(files);
+	const formData = new FormData();
+	for(let i = 0; i<files.length; i++){
+		formData.append(`files`, files[i]);
+	}
+	return formData;
+}
 
+const HTTPRequest = async function(url, data , options){
+	console.log(options);
+	const response = await axios.post(url, data, options);
+	const json = await response.json();
+	console.log(await json);
 };
 
 
