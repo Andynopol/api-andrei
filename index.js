4e1864f5e05b16a9a258eb2639032c7d75dd2902
Andynopol <andrei.nechita95@gmail.com>
2020-11-05 21:02:36 +0200

    ok

--- index.js
+++ index.js
@@ -1,143 +1,122 @@
-const express = require( 'express' );
-const Datastore = require( 'nedb' );
-const base64_manager = require( './lib/base64_manager' );
-const File = require( './lib/file' );
-const upload = require( 'express-fileupload' );
-const cors = require( 'cors' );
+const express = require("express");
+const Datastore = require("nedb");
+const base64_manager = require("./lib/base64_manager");
+const File = require("./lib/file");
+const upload = require("express-fileupload");
+const cors = require("cors");
 
 // Init app
 
 const INTERVAL = 1000 * 60 * 60 * 24;
 
-require( 'dotenv' ).config();
-
-
+require("dotenv").config();
 
 const app = express();
-const database = new Datastore( 'data.db' );
-database.persistence.setAutocompactionInterval( INTERVAL );
+const database = new Datastore("data.db");
+database.persistence.setAutocompactionInterval(INTERVAL);
 database.loadDatabase();
 
-
 const PORT = process.env.API_PORT || 3000;
-console.log( PORT );
+console.log(PORT);
 
+app.use(cors());
 
-app.use( cors() );
+app.listen(PORT, () => console.log("I am listening"));
 
-app.listen( PORT, () => console.log( 'I am listening' ) );
+app.use(express.static("./UI"));
+app.use(
+  express.json({
+    limit: "1mb",
+  })
+);
 
+app.use(upload());
 
-app.use( express.static( './UI' ) );
-app.use( express.json( {
-	limit: '1mb'
-} ) );
+app.get("/files/:command", (req, res) => {
+  if (req.params.command === "all") {
+    database.find({}, (err, data) => {
+      if (err) {
+        res.json({
+          status: "Internal Error",
+        });
+        res.end();
+        return;
+      }
+      base64_manager.convert(data);
+      res.json(data);
+    });
+  }
+});
 
-app.use( upload() );
+app.post("/files", (req, res) => {
+  if (req.files) {
+    const files = req.files;
+    try {
+      for (let item in files) {
+        const data = {};
+        const file = files[item];
+        console.log(file);
+        const fileName = file.name;
+        data.name = fileName;
+        data.path = "./uploads/" + fileName;
+        deleteDuplicates({
+          path: data.path,
+        });
+        data.type = file.mimetype;
+        data.size = file.size;
+        data.md5 = file.md5;
+        data.timestamp = Date.now();
+        File.write(data.path, file.data);
+        database.insert(data);
+      }
+      res.json({
+        statsu: "success",
+      });
+      res.end();
+    } catch (err) {
+      try {
+        for (let item of files.files) {
+          const data = {};
+          console.log("file2: " + item);
+          console.log("data: " + data);
+          const fileName = item.name;
+          data.name = fileName;
+          data.path = "./uploads/" + fileName;
+          deleteDuplicates({
+            path: data.path,
+          });
+          data.type = item.mimetype;
+          data.size = item.size;
+          data.md5 = item.md5;
+          data.timestamp = Date.now();
+          console.log(data);
+          File.write(data.path, item.data);
+          database.insert(data);
+        }
+        res.json({
+          statsu: "success",
+        });
+        res.end();
+      } catch (err) {
+        res.json({
+          statsu: err,
+        });
+        res.end();
+      }
+    }
+  } else {
+    res.end();
+  }
+});
 
-app.get( '/files/:command', ( req, res ) => {
-	if ( req.params.command === 'all' )
-	{
-		database.find( {}, ( err, data ) => {
-			if ( err )
-			{
-				res.json( {
-					'status': 'Internal Error'
-				} );
-				res.end();
-				return;
-			}
-			base64_manager.convert( data );
-			res.json( data );
-		} );
-	}
-
-} );
-
-app.post( '/files', ( req, res ) => {
-	if ( req.files )
-	{
-		const files = req.files;
-		try
-		{
-			for ( let item in files )
-			{
-				const data = {};
-				const file = files[ item ];
-				console.log( file );
-				const fileName = file.name;
-				data.name = fileName;
-				data.path = './uploads/' + fileName;
-				deleteDuplicates( {
-					path: data.path
-				} );
-				data.type = file.mimetype;
-				data.size = file.size;
-				data.md5 = file.md5;
-				data.timestamp = Date.now();
-				File.write( data.path, file.data );
-				database.insert( data );
-			}
-			res.json( {
-				statsu: "success"
-			} );
-			res.end();
-		} catch ( err )
-		{
-			try
-			{
-				for ( let item of files.files )
-				{
-					const data = {};
-					console.log( "file2: " + item );
-					console.log( "data: " + data );
-					const fileName = item.name;
-					data.name = fileName;
-					data.path = './uploads/' + fileName;
-					deleteDuplicates( {
-						path: data.path
-					} );
-					data.type = item.mimetype;
-					data.size = item.size;
-					data.md5 = item.md5;
-					data.timestamp = Date.now();
-					console.log( data );
-					File.write( data.path, item.data );
-					database.insert( data );
-				}
-				res.json( {
-					statsu: "success"
-				} );
-				res.end();
-			} catch ( err )
-			{
-				res.json( {
-					statsu: err
-				} );
-				res.end();
-			}
-		}
-
-
-	} else
-	{
-		res.end();
-	}
-} );
-
-
-
-const deleteDuplicates = function ( query ) {
-	database.remove( query, {}, ( err, num ) => {
-		if ( err )
-		{
-			console.log( err );
-		} else
-		{
-			if ( num !== 0 )
-			{
-				console.log( num );
-			}
-		}
-	} );
+const deleteDuplicates = function (query) {
+  database.remove(query, {}, (err, num) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (num !== 0) {
+        console.log(num);
+      }
+    }
+  });
 };
