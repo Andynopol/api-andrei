2325591ccceede9ed15ca2bf3cbed7e916e35fe0
Andynopol <andrei.nechita95@gmail.com>
2020-11-05 20:25:33 +0200

    new PORT

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ database.persistence.setAutocompactionInterval( INTERVAL );
 database.loadDatabase();
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.API_PORT || 3000;
 console.log( PORT );
 
 
